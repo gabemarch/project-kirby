7a46a0976a7a2161101a29cf9bd97c3704620da3
Gabor Szilagyi <szilagyi_gabor@outlook.com>
2022-01-09 08:09:34 +0100

    added new components

---
+++ src/components/ItemSocialBox/ItemSocialBox.tsx
@@ -0,0 +1,15 @@
+const ItemSocialBox = () => {
+  return (
+    <div className="item-social-box">
+      {/* TODO: TOGGLE LIKE COMPONENT */}
+      <p>Like | Liked</p>
+      <p>Read</p>
+      <p>Wishlist | Wishlisted</p>
+      <div className="item-social-box-comment-box">
+        {/* TODO: COMMENT COMPONENT */}
+      </div>
+    </div>
+  )
+}
+
+export default ItemSocialBox;
